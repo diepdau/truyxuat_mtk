fcfbb10ca3e81336b23a2d21ac5756dd0aaf9d1a
DauThiTieuDiep <2115196@dlu.edu.vn>
2024-05-25 21:08:43 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './store/reducers/rootReducer.js'
-
 import { AuthContexProvider} from "./asset/service/index.js";
-import { SidebarProvider } from '../src/components/Sidebar/SidebarContext.js'; 
-const reduxStore=createStore(rootReducer);
+import { SidebarProvider } from '../src/components/Sidebar/SidebarContext.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={reduxStore}>
     <AuthContexProvider>
      <SidebarProvider>
       <App />
       </SidebarProvider>
      </AuthContexProvider>
-    </Provider>
     
   </React.StrictMode>
 );
